ae19bfa7d457f9b12d5a2075e702ca3add172ae5
Daniel Xavier <danielxavier.job@gmail.com>
2023-02-27 10:08:49 -0300

    🎉 init: theme mode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import IconButton from "@mui/material/IconButton";
+import Box from "@mui/material/Box";
+import { useTheme, ThemeProvider, createTheme } from "@mui/material/styles";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
 
-function App() {
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+
+function MyApp() {
+  const theme = useTheme();
+  const colorMode = React.useContext(ColorModeContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      sx={{
+        display: "block",
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+        bgcolor: "background.default",
+        color: "text.primary",
+        borderRadius: 1,
+        p: 3,
+      }}
+    >
+      {theme.palette.mode} mode
+      <IconButton
+        sx={{ ml: 1 }}
+        onClick={colorMode.toggleColorMode}
+        color="inherit"
+      >
+        {theme.palette.mode === "dark" ? (
+          <Brightness7Icon />
+        ) : (
+          <Brightness4Icon />
+        )}
+      </IconButton>
+      {theme.palettue.backgrounds.map((item, i) => {
+        return (
+          <p key={i}>
+            {item.name} - {item.background}
+          </p>
+        );
+      })}
+    </Box>
   );
 }
 
-export default App;
+export default function ToggleColorMode() {
+  const lightMode = {
+    mode: "light",
+    backgrounds: [
+      {
+        name: "navbar",
+        background: "#fff9",
+      },
+      {
+        name: "sidebar",
+        background: "#fff8",
+      },
+    ],
+  };
+  const darkMode = {
+    mode: "dark",
+    backgrounds: [
+      {
+        name: "navbar",
+        background: "#003",
+      },
+      {
+        name: "sidebar",
+        background: "#002",
+      },
+    ],
+  };
+  const [mode, setMode] = React.useState(lightMode);
+
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) =>
+          prevMode.mode === "light" ? darkMode : lightMode
+        );
+      },
+      mode,
+    }),
+    []
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          ...mode
+        },
+      }),
+    [mode]
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <MyApp />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
